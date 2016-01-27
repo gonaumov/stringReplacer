fd6fd1ae7255ca885036009634e21a6b9936410d
Georgi Naumov <gonaumov@gmail.com>
2016-01-27 16:31:32 +0200

    Remove unnecessary describe block.

--- spec/ReplacerSpec.js
+++ spec/ReplacerSpec.js
@@ -1,31 +1,29 @@
-describe("Test the two functions replaceStringWithNonCapturingGroup and replaceStringWithCharacterClass. ", function () {
-    describe("replaceString must working properly with all symbols including newlines ", function () {
-        it('must working properly without newlines', function () {
-            var input = ['"START";',
-                'var settings ={"dev":false,"production":"n","id":"bcdef","key":"1234xyz","secret":"22sdfas"}',
-                '"END"'].join('');
-            var expected = '"START";var __REPLACED = console.log(\'replaced\');"END"';
-            var result = replaceString(input);
+describe("replaceString must working properly with all symbols including newlines ", function () {
+    it('must working properly without newlines', function () {
+        var input = ['"START";',
+            'var settings ={"dev":false,"production":"n","id":"bcdef","key":"1234xyz","secret":"22sdfas"}',
+            '"END"'].join('');
+        var expected = '"START";var __REPLACED = console.log(\'replaced\');"END"';
+        var result = replaceString(input);
 
-            expect(result).toEqual(expected);
-        });
+        expect(result).toEqual(expected);
+    });
 
-        it('must working properly with newlines', function () {
-            var input = ['"START";',
-                "\n",
-                'var settings ={"dev":false,"production":"n","id":"bcdef","key":"1234xyz","secret":"22sdfas"}',
-                '"END"'].join('');
-            var expected = '"START";var __REPLACED = console.log(\'replaced\');"END"';
-            var result = replaceString(input);
+    it('must working properly with newlines', function () {
+        var input = ['"START";',
+            "\n",
+            'var settings ={"dev":false,"production":"n","id":"bcdef","key":"1234xyz","secret":"22sdfas"}',
+            '"END"'].join('');
+        var expected = '"START";var __REPLACED = console.log(\'replaced\');"END"';
+        var result = replaceString(input);
 
-            expect(result).toEqual(expected);
-        });
+        expect(result).toEqual(expected);
+    });
 
-        it('must not change string when "START" and "END" is not present', function () {
-            var input = 'example input';
-            var result = replaceString(input);
+    it('must not change string when "START" and "END" is not present', function () {
+        var input = 'example input';
+        var result = replaceString(input);
 
-            expect(result).toEqual(input);
-        });
+        expect(result).toEqual(input);
     });
 });
